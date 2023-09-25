04001f684b2f8ae554c546f3f8a98c30f65a1245
Hiokyo <hieutran235@gmail.com>
2023-09-25 15:35:46 +0700

    feat: implement banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,117 @@
-import React from 'react'
+import React from 'react';
+import avatar from '../assets/avatar.png';
+import { FaGithub, FaFacebook, FaLinkedin } from 'react-icons/fa';;
+import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+
 
 const Banner = () => {
   return (
-    <div className='section' id='home'>Banner</div>
+    <section 
+      className='min-h-[85vh] lg:min-h[78vh] flex items-center' 
+      id='home'
+    >
+      <div className="container mx-auto">
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+          {/* Text */}
+          <div className='flex-1 text-center font-secondary lg:text-left'>
+            <motion.h1 
+              variants={fadeIn('up', 0.3)}
+              initial={{opacity: 0, y: 100}}
+              whileInView={'show'}
+              viewport={{
+                once: false,
+                amount: 0.7
+              }}
+              className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'
+            >
+              HIEU 
+              <span>
+                TRAN
+              </span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn('up', 0.4)}
+              initial={{opacity: 0, y: 100}}
+              whileInView={'show'}
+              viewport={{
+                once: false,
+                amount: 0.7
+              }}
+              className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'
+            >
+              <span className='text-white mr-3'>I am a</span>
+              <TypeAnimation 
+                sequence={[
+                  'Developer',
+                  2000
+                ]}
+                speed={50}
+                className='text-accent'
+                wrapper='span'
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p
+              variants={fadeIn('up', 0.5)}
+              initial={{opacity: 0, y: 100}}
+              whileInView={'show'}
+              viewport={{
+                once: false,
+                amount: 0.7
+              }}
+              className='mb-8 max-w-lg mx-auto lg:mx-0'
+            >
+              Aspire to be a professional Frontend developer through using attained technical knowledge and intense zeal for working in software programming field.
+              Additionally, complementing other values to myself in order to accumulate much experiences for future promotion
+            </motion.p>
+            <motion.div
+              variants={fadeIn('up', 0.6)}
+              initial={{opacity: 0, y: 100}}
+              whileInView={'show'}
+              viewport={{
+                once: false,
+                amount: 0.7
+              }}
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'
+            >
+              <button className='btn btn-lg'>Contact me</button>
+              <a href="#" className='text-gradient btn-link'>My Resume</a>
+            </motion.div>
+            {/* Social */}
+            <motion.div
+               variants={fadeIn('up', 0.7)}
+               initial={{opacity: 0, y: 100}}
+               whileInView={'show'}
+               viewport={{
+                 once: false,
+                 amount: 0.7
+               }} 
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+              <a href="https://www.facebook.com/hiokyo96" target="_blank">
+                <FaFacebook />
+              </a>
+              <a href="https://github.com/Hiokyo" target="_blank">
+                <FaGithub />
+              </a>
+              <a href="https://www.linkedin.com/in/hieutran235/" target="_blank">
+                <FaLinkedin />
+              </a>
+            </motion.div>
+          </div>
+          {/* Avatar */}
+          <motion.div
+            variants={fadeIn('down', 0.5)}
+            initial={{opacity: 0, y: -100}}
+            whileInView={'show'}
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'
+          >
+            <img src={avatar} alt='' />
+          </motion.div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <div className='bg-pink-300'>Header</div>
+    <header
+      className='py-8'
+    >
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          <a href='#'>
+            <img className='w-40 h-auto' src={logo} alt=''></img>
+          </a>
+          
+          <button className='btn btn-sm'> Work with me</button>
+
+        </div>
+      </div>
+    </header>
   )
 }
 
