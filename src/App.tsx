e007134175c9f9052690e5f6ba2d37f229e40910
Hiokyo <hieutran235@gmail.com>
2023-09-29 20:16:41 +0700

    fix: test vercel config

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Services from "./components/Services"
 import Work from "./components/Work"
 
 function App() {
-
   return (
     <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
       <Header />
